c2846d3c9512ab8c5a05f6a535cf1c48ae3b6bb6
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-06-22 23:17:32 +0500

    Nav bar completed

---
+++ src/Components/HomeNav/HomeNav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './HomeNav.css'
+function HomeNav() {
+    return (
+        <div className="homeNav_wrapper">
+            <div className="tagName flex"><h1 className="heading3 white bolder">M.Hunbal Siddiqui</h1></div>
+
+            <div className="subscription flex-col">
+                <h1 className="heading3 white lighter">Subscribe for newsletter</h1>
+            <form>
+                <input className="para1" type="email" name="" id="subscribtion_email" placeholder="Email" required/>
+                <button className="para1 whiteBack pointer subscibeBtn"><i class="far fa-paper-plane"></i></button>
+            </form>
+            </div>
+
+            <div className="nav_footer flex">
+                <h1 className="para4 white">COPYRIGHT © 2020 Muhammad Hunbal Siddiqui - ALL RIGHTS RESERVED</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default HomeNav
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Home.css'
+import HomeNav from '../../Components/HomeNav/HomeNav'
 function Home() {
     return (
         <div className="home_wrapper">
             <div className="home_cont">
-                <div className="home_nav_area"></div>
+                <div className="home_nav_area">
+                    <HomeNav/>
+                </div>
                 <div className="search_filters"></div>
                 <div className="home_blogs_area"></div>
             </div>
