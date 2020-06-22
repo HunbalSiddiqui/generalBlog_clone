9dc7abe04ec10edc870c8e918bf4bd5b66436ddd
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-06-22 22:47:21 +0500

    InitialSetup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+    return (
+        <div className="home_wrapper">
+            <div className="home_cont">
+                <div className="home_nav_area"></div>
+                <div className="search_filters"></div>
+                <div className="home_blogs_area"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
