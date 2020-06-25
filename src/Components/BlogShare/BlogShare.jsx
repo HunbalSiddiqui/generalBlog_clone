0a9bea5bc6800eefed48fbf2184c1bc754a22f17
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-06-25 19:39:12 +0500

    Viewing Blog Done

--- src/Components/BlogShare/BlogShare.jsx
+++ src/Components/BlogShare/BlogShare.jsx
@@ -3,8 +3,8 @@ import './BlogShare.css'
 function BlogShare() {
     return (
         <div className="BlogShare_wrapper flex">
-            <h1 className="heading3 social_icon flex lightblue"><i class="fab fa-facebook-f pointer"></i></h1>
-            <h1 className="heading3 social_icon flex lightblue"><i class="fab fa-twitter pointer"></i></h1>
+            <h1 className="heading3 social_icon flex lightblue"><i className="fab fa-facebook-f pointer"></i></h1>
+            <h1 className="heading3 social_icon flex lightblue"><i className="fab fa-twitter pointer"></i></h1>
         </div>
     )
 }
--- src/Pages/ViewBlog/ViewBlog.jsx
+++ src/Pages/ViewBlog/ViewBlog.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './ViewBlog.css'
 import HomeNav from '../../Components/HomeNav/HomeNav'
 import BlogShare from '../../Components/BlogShare/BlogShare'
+import {connect} from 'react-redux'
+import { AllBlogs } from '../../Util/Data'
+// import {Redirect} from 'react-router-dom'
+function ViewBlog(props) {
+    console.log(props.blogId)
+    console.log(AllBlogs)
 
-function ViewBlog() {
+    var [title,setTitle] = useState(null)
+    var [image,setimage] = useState(null)
+    var [content,setContent] = useState(null)
+    var [category,setCategory] = useState(null)
+
+    useEffect(()=>{
+        if(props.blogId)
+        {
+            //for each will be unoptimized for large bulk of blogs...as break cant be used in it
+            AllBlogs.forEach(element => {
+                if(element.uid===props.blogId)
+                {
+                    setTitle(element.title);
+                    setimage(element.img);
+                    setContent(element.content);
+                    setCategory(element.category);
+                }
+            });   
+        }
+        else{
+            props.history.push('/');
+        }
+    },[props.blogId])
+
     return (
         <div className="viewblog_wrapper">
             <div className="viewblog_cont">
@@ -13,9 +42,21 @@ function ViewBlog() {
                 <div className="viewblog_header">
                     <BlogShare/>
                 </div>
+                <div className="blog_view">
+    <div className="title heading4"><h1 className='lighter'>{title}</h1></div>
+    <div className="blog_image flex"><img className="image_setting" src={image} alt="" srcset=""/></div>
+    <div className="story"><h1 className='para1 lighter'>{content}</h1></div>
+                </div>
+
             </div>
         </div>
     )
 }
 
-export default ViewBlog
+var mapStateToProps = (state) =>{
+    return{
+        blogId : state.userReducer.blogId
+    }
+}
+
+export default connect(mapStateToProps)(ViewBlog)
